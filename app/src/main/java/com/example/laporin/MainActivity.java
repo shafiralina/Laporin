c727db99a44e2e5f4206f447fbbabee62652fc5e
Shafira Herlinawati <shafiralina17@gmail.com>
2020-04-14 20:35:26 +0700

    Layar awal

---
+++ app/src/main/java/com/example/laporin/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.laporin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+        setContentView(R.layout.activity_main);
+    }
+}
