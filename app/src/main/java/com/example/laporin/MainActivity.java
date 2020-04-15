58f54e0cd04a4a5a2553bdbb0932d9ebc81f3fea
aldiansyah.ramadlan <aldiansyah.ramadlan@multipolar.com>
2020-04-15 14:09:35 +0700

    menu kotor

--- app/src/main/java/com/example/laporin/MainActivity.java
+++ app/src/main/java/com/example/laporin/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.laporin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+
+    Button buttonLapor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,19 @@ public class MainActivity extends AppCompatActivity {
         }
         catch (NullPointerException e){}
         setContentView(R.layout.activity_main);
+
+        buttonLapor = findViewById(R.id.button3);
+        buttonLapor.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button3:
+                Intent moveToMenu = new Intent(this,MenuActivity.class);
+                startActivity(moveToMenu);
+                break;
+        }
+
     }
 }
