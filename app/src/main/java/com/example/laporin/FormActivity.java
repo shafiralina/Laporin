473dde2cd2c65eef91648b234578cd9a16404d01
aldiansyah.ramadlan <aldiansyah.ramadlan@multipolar.com>
2020-04-19 17:56:50 +0700

    form with kamera

--- app/src/main/java/com/example/laporin/FormActivity.java
+++ app/src/main/java/com/example/laporin/FormActivity.java
@@ -1,14 +1,25 @@
 package com.example.laporin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.core.content.res.ResourcesCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -18,14 +29,19 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class FormActivity extends AppCompatActivity implements View.OnClickListener{
-    TextView textForm;
+    TextView textForm, textGambar;
     Spinner spinner;
-    Button buttonCek, buttonKirimLaporan;
+    Button buttonCek, buttonKirimLaporan, buttonKamera;
     EditText formKoordinat;
+    ImageView gambarFoto;
+    private Uri file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -116,7 +132,15 @@ public class FormActivity extends AppCompatActivity implements View.OnClickListe
         buttonKirimLaporan = findViewById(R.id.buttonKirimLaporan);
         buttonKirimLaporan.setOnClickListener(this);
 
+        buttonKamera = findViewById(R.id.buttonKamera);
+        buttonKamera.setOnClickListener(this);
+
+        gambarFoto = findViewById(R.id.gambar_hasil);
+
+        textGambar = findViewById(R.id.textGambar);
+
         formKoordinat = findViewById(R.id.edt_koordinat);
+
     }
 
     @Override
@@ -124,7 +148,7 @@ public class FormActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.buttonCek:
                 formKoordinat.setText("-7.2413975,112.7322592");
-            break;
+                break;
 
             case R.id.buttonKirimLaporan:
                 CharSequence text = "Aplikasi sedang dalam pengembangan";
@@ -132,7 +156,83 @@ public class FormActivity extends AppCompatActivity implements View.OnClickListe
 
                 Toast toast = Toast.makeText(this, text, duration);
                 toast.show();
+                break;
+
+            case R.id.buttonKamera:
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    buttonKamera.setEnabled(false);
+                    Toast toastt = Toast.makeText(this, "kamera mati",  Toast.LENGTH_SHORT);
+                    toastt.show();
+                    ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+                }else{
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    file = Uri.fromFile(getOutputMediaFile());
+                    File cameraPhoto = getOutputMediaFile();
+                    Uri photoUri = FileProvider.getUriForFile(
+                            this,
+                            getPackageName() + ".fileprovider",
+                            cameraPhoto);
+                    Log.d("photoUri-aldi",  photoUri.getPath());
+                    file = photoUri;
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                    startActivityForResult(intent, 100);
+                }
+
         }
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                buttonKamera.setEnabled(true);
+                Toast toast = Toast.makeText(this, "kamera nyala",  Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+
+    public void takePicture(View view) {
+        Toast toastt = Toast.makeText(this, "kamera nyala",  Toast.LENGTH_SHORT);
+        toastt.show();
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        file = Uri.fromFile(getOutputMediaFile());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+
+        startActivityForResult(intent, 100);
+    }
+
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                Log.d("CameraDemo", "failed to create directory");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                gambarFoto.setImageURI(file);
+                textGambar.setText("");
+                Toast toast = Toast.makeText(this, "berhasil ambil foto",  Toast.LENGTH_SHORT);
+                toast.show();
+
+            }
+        }
+    }
+
 }
